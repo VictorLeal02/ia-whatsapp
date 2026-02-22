59dff1f10a37bbfdd50bf0e9e4cc146f34b3bcef
Victor Leal <infoeclima2009@gmail.com>
2026-02-22 19:36:49 -0300

    fix: use PORT do Railway

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.post("/webhook", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Servidor rodando na porta", PORT);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Servidor rodando na porta " + PORT);
 });
