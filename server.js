0f66a013a6ff1e58421d78e903abd853dcd4c648
Victor Leal <infoeclima2009@gmail.com>
2026-02-22 19:12:11 -0300

    primeira versão

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("IA InfoeClima online 🚀");
+});
+
+app.post("/webhook", (req, res) => {
+  console.log(req.body);
+
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando");
+});
