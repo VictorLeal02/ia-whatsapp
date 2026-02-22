ce03194406c29eaa8517c329d79237c199a031cd
Victor Leal <infoeclima2009@gmail.com>
2026-02-22 19:23:42 -0300

    fix porta railway

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ app.get("/", (req, res) => {
 
 app.post("/webhook", (req, res) => {
   console.log(req.body);
-
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta", PORT);
 });
