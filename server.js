9d6d998aa8b5fbbdff21a834b0c8f1b32d414ea7
Victor Leal <infoeclima2009@gmail.com>
2026-02-22 21:40:44 -0300

    codigo funcionando

--- server.js
+++ server.js
@@ -2,66 +2,50 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-
 app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
 
-
-// DADOS DA Z-API
-const INSTANCE_ID = "3EF2706B104CF2716C3C3A4A6B9BCAAE";
+// DADOS Z-API
+const INSTANCE_ID = "3EF2706B104CF2716C3A4A6B9BCAE";
 const TOKEN = "0761342768AE2DA4D80F8727";
 
-
+// WEBHOOK
 app.post("/webhook", async (req, res) => {
+
+    console.log("Mensagem recebida:");
+
+    const mensagem = req.body.text?.message;
+    const telefone = req.body.phone;
+
+    console.log("Telefone:", telefone);
+    console.log("Mensagem:", mensagem);
+
+    if (!mensagem) {
+        return res.sendStatus(200);
+    }
 
     try {
 
-        console.log("Mensagem recebida:");
-
-        const mensagem = req.body.text?.message;
-        const telefone = req.body.phone;
-
-        console.log("Telefone:", telefone);
-        console.log("Mensagem:", mensagem);
-
-
-        if (!mensagem) {
-            return res.sendStatus(200);
-        }
-
-
-        let resposta = "Olá! Sou a Info&Clima 👋";
-
-        if (mensagem.toLowerCase().includes("oi")) {
-            resposta = "Olá! 👋\nInfo&Clima aqui.\nPrecisa de ar condicionado ou energia solar?";
-        }
-
-
-        // URL CORRETA DA Z-API
         await axios.post(
             `https://api.z-api.io/instances/${INSTANCE_ID}/token/${TOKEN}/send-text`,
             {
                 phone: telefone,
-                message: resposta
+                message: "Recebi sua mensagem: " + mensagem
             }
         );
 
-
-        console.log("Resposta enviada!");
-
-        res.sendStatus(200);
+        console.log("Resposta enviada");
 
     } catch (erro) {
 
-        console.log("Erro:", erro.response?.data || erro.message);
-
-        res.sendStatus(200);
+        console.log("Erro envio:", erro.response?.data || erro.message);
 
     }
 
-});
+    res.sendStatus(200);
 
+});
 
 app.listen(PORT, () => {
     console.log("Servidor rodando na porta", PORT);
